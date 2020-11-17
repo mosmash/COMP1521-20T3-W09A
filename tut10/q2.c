27230f85c6c4f585ce08c1cdb63c4c881999708c
natLF3RD <natiqq@icloud.com>
2020-11-18 10:44:36 +1100

    week 10

---
+++ tut10/q2.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+    char *home = getenv("HOME");
+    if (home == NULL) {
+        fprintf(stderr, "HOME variable failed");
+    }
+
+
+    // combine .diary and home location string
+    char* diary = "/.diary";
+    size_t n_bytes = strlen(home) + strlen(diary) + 1;
+    char dest[n_bytes];
+    snprintf(dest, n_bytes, "%s%s", home, diary);
+
+    FILE *fp= fopen(dest, "r");
+    if (fp == NULL {
+        perror(dest);
+    }
+
+    int c = fgetc(fp);
+    while (c != EOF) {
+        fputc(c, stdout);
+        c = fgetc(fp);
+    }
+
+    return 0;
+}
